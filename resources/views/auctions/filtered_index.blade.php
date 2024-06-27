65b9960c9b98cbbc309e2232f4e6c9a72ee89d1a
dariovidovic <malivida00@gmail.com>
2024-06-27 18:10:42 +0200

    added routing to auctions info page

--- resources/views/auctions/filtered_index.blade.php
+++ resources/views/auctions/filtered_index.blade.php
@@ -1,66 +1,53 @@
+<style>
+    .photo-size{
+        width: auto;
+        height: 250px;
+        object-fit: cover; 
+    }
+
+    .card button {
+        background: none;
+        border: none;
+        outline: none;
+        text-align: center;
+        padding: 0;
+    }
+
+    .card:hover {
+        background-color: #f0f0f0; 
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+</style>
 <div class="row">
     <div class="col-md-12 d-flex justify-content-center">
         @foreach ($auctions as $auction)
             <div class="card mb-3 w-75">
-                <div class="card-body">
-                    <h1 class="card-title fw-bold">{{ $auction->item_name }}</h1>
-                        <div class="d-flex flex-row">
-
-                            <div class="d-flex flex-column me-2">
-                                <p class="card-text text-secondary fw-bold">AUCTION ID</p>
-                                <p class="fw-bold">#{{ $auction->id }} </p>
-                            </div>
-
-                            <div class="d-flex flex-column me-2">
-                                <p class="card-text text-secondary fw-bold">CREATED BY</p>
-                                <p class="fw-bold"> {{ $auction->creator->name }} </p>
-                            </div>
-
-                            <div class="d-flex flex-column me-2">
-                                <p class="card-text text-secondary fw-bold">ENDING</p>
-                                <p class="fw-bold"> {{ \Carbon\Carbon::parse($auction->end_time)->format('d-m-Y H:i') }} </p>
-                            </div>
-
-                        </div>
-
-                        <div class="d-flex flex-row">
-                            <div>
-                                @if ($auction->photo)
-                                <img src="{{ asset('storage/photos/' . $auction->photo) }}" alt="{{ $auction->item_name }}" class="img-fluid photo-size">
-                                @endif
-                            </div>
-
-                            <div class="ms-5">
-                                <p class="card-text fs-2">Starting Price: ${{ number_format($auction->starting_price, 2) }}</p>
-                                <p class="card-text fs-2">Buyout Price: ${{ number_format($auction->buyout_price, 2) }}</p>
-                                
-                                @if (Auth::id() != $auction->creator_id)
-                                <div class="d-flex flex-row">
-                                    <div class="input-group mb-3 w-50">
-                                        <span class="input-group-text bg-body-secondary">€</span>
-                                        <input type="text" class="form-control w-25">
-                                    </div>
-                                    <button type="submit" class="btn btn-secondary ms-2 h-25">Bid</button>
+                        <button onclick="window.location='{{ route('auctions.show', ['auction' => $auction]) }}'">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-center">
+                                    <h1 class="card-title fw-bold">{{ $auction->item_name }}</h1>
                                 </div>
-                                @endif
-                                
-                            </div>
-                       
-                        </div>
-                    
-                    <div class="d-flex flex-column mt-2">
-                                <p class="card-text fw-bold">AUCTION DESCRIPTION</p>
-                                <p> {{ $auction->item_description }} </p>
-                    </div>
+                            
+                                <div class="d-flex flex-row justify-content-center">
+                                    <div>
+                                        @if ($auction->photo)
+                                        <img src="{{ asset('storage/photos/' . $auction->photo) }}" alt="{{ $auction->item_name }}" class="img-fluid photo-size">
+                                        @endif
+                                    </div>
+                                </div>
 
-                    @if (Auth::id() == $auction->creator_id)
-                    <form action="{{ route('auctions.destroy', $auction->id) }}" method="POST" class="mt-2">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
-                    @endif
-                </div>
+                                <div class="d-flex flex-row me-2 justify-content-center mt-2">
+                                        <p class="card-text text-secondary fw-bold me-2 mb-0">ENDING</p>
+                                        <p class="fw-bold mb-0"> {{ \Carbon\Carbon::parse($auction->end_time)->format('d-m-Y H:i') }} </p>
+                                </div>
+
+                                <div class="d-flex justify-content-center">
+                                        <p class="card-text fs-2">${{ number_format($auction->starting_price, 2) }}</p>              
+                                </div>
+                            </div>
+                        <button>
             </div>
         @endforeach
     </div>
